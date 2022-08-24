4c8e2c986d6e1af1c3ab8257341b5d67994c5339
Anurag Chauhan <anuragchauhan@Anurags-MacBook-Pro.local>
2022-08-24 13:43:55 +0530

    Add metamap plugin button and functionality

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { MetaMapCapacitor } from "metamap-capacitor-plugin";
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
   constructor() {}
+
+  showMetaMapFlow() {
+      let metadataParams = { param1: "value1" };
+      let registerParams = { clientId: "5c94e3c401ddc6001be83c07", flowId: "5e962a23728ddc001b5937aa", metadata: metadataParams};
+
+      MetaMapCapacitor.showMetaMapFlow(registerParams)
+        .then( verification => console.log("verification success:" + verification.verificationID))
+        .catch(() => console.log("verification cancelled"))
+    }
 
 }
